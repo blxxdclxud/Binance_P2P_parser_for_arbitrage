ab691be86ad84a5a32669d63b2a1f9a664bdfb65
blxxdclxud <ramazanatzuf10@gmail.com>
2022-09-29 18:20:01 +0300

    update: version for server done

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+from flask import Blueprint, Flask
 from google_sheets import start_spreadsheet
 
-if __name__ == '__main__':
+app = Flask(__name__)
+p2p_parser = Blueprint(
+    'p2p_parser',
+    __name__
+)
+
+
+@p2p_parser.route("/post", methods=["POST"])
+def main_post():
+    print(11)
     start_spreadsheet()
+
+
+if __name__ == '__main__':
+    app.register_blueprint(p2p_parser)
+    app.run(host='localhost', port=5000)
